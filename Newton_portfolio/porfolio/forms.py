c4ca2359172c5d7fc5ca983806ff561f5fc3799d
unknown <armstrongnewton02@gmail.com>
2024-11-24 22:30:18 +0300

    Added my portfolio successfully

--- Newton_portfolio/porfolio/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import CustomMessage
-
-class CustomMessageForm(forms.ModelForm):
-    class Meta:
-        model = CustomMessage
-        fields = ['name', 'email', 'message']
